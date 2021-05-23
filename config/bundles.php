253dc73881feca1f8f26b49f26f6547646d57413
winni4eva <adams4lyf@gmail.com>
2021-05-23 20:25:44 +0000

    Adds home template inputs

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];

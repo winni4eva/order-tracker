80c18a4617aee801c465067add8b74915c6b4e71
winni4eva <adams4lyf@gmail.com>
2021-05-24 12:26:21 +0000

    Abstracted order form processing logic

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,23 +21,7 @@ class HomeController extends AbstractController
         
         if ($form->isSubmitted() && $form->isValid()) {
             $orderFormData = $form->getData();
-            $orderFormData['state'] = 'ORDER_RECEIVED';
-            $orderFormData['discount'] = '0';
-            $orderFormData['total'] = (int)($orderFormData['itemPrice'] * 100);
-            
-            $orderFormData['orderItems'][] = [
-                'name' => $orderFormData['itemName'],
-                'price' => (int)($orderFormData['itemPrice'] * 100),
-                'quantity' => (int)$orderFormData['itemQuantity']
-            ];
-            $orderFormData['orderShippingDetail'] = [
-                'country' => $orderFormData['country'],
-                'state' => $orderFormData['state'],
-                'zip' => $orderFormData['zip'],
-                'street' => $orderFormData['street'],
-                'phone' => $orderFormData['phone'],
-            ];
-            $orderService->saveOrder($orderFormData);
+            $orderService->saveOrder($this->processFormData($orderFormData));
             unset($form);
             $form = $this->createForm(OrderType::class);
         }
@@ -47,6 +31,28 @@ class HomeController extends AbstractController
             'orders' => $orders,
             'form' => $form->createView(),
         ]);
+    }
+
+    private function processFormData(&$orderFormData): array
+    {
+        $orderFormData['state'] = 'ORDER_RECEIVED';
+        $orderFormData['discount'] = '0';
+        $orderFormData['total'] = (int)($orderFormData['itemPrice'] * 100);
+        
+        $orderFormData['orderItems'][] = [
+            'name' => $orderFormData['itemName'],
+            'price' => (int)($orderFormData['itemPrice'] * 100),
+            'quantity' => (int)$orderFormData['itemQuantity']
+        ];
+        $orderFormData['orderShippingDetail'] = [
+            'country' => $orderFormData['country'],
+            'state' => $orderFormData['state'],
+            'zip' => $orderFormData['zip'],
+            'street' => $orderFormData['street'],
+            'phone' => $orderFormData['phone'],
+        ];
+
+        return $orderFormData;
     }
 
 }

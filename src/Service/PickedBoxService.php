3840f4a05f63c5a6300ec222ef746f97e73f4bca
winni4eva <adams4lyf@gmail.com>
2021-06-01 22:28:05 +0000

    Fixed picker order complete user query bug

--- src/Service/PickedBoxService.php
+++ src/Service/PickedBoxService.php
@@ -8,28 +8,33 @@ use App\Entity\PickedBox;
 use App\Repository\OrderRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
 
 class PickedBoxService
 {
 
     private $entityManager;
 
+    private $security;
+
     protected $orderRepository;
 
     public function __construct(
         EntityManagerInterface $entityManager,
         UserRepository $userRepository,
-        OrderRepository $orderRepository
+        OrderRepository $orderRepository,
+        Security $security
     )
     {
         $this->entityManager = $entityManager;
         $this->userRepository = $userRepository;
         $this->orderRepository = $orderRepository;
+        $this->security = $security;
     }
 
     public function saveBoxId( int $orderId, string $boxId): void
     {
-        $user = $this->userRepository->findOneByRoleField('PICKER');
+        $user = $user = $this->security->getUser();
         $order = $this->orderRepository->findOneById($orderId);
 
         $pickedBox = new PickedBox();
